20729679a4b41b11deb12957be007cb02fcf04f0
Hoang Viet Dung <dung.hvd@gmail.com>
2017-05-23 22:50:29 +0700

    update autoload path

--- app/task1/matrix_multiplication.php
+++ app/task1/matrix_multiplication.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 use Algorithm\Matrix\Formatter\MatrixFormatter;
 use Algorithm\Matrix\Generator\MatrixGenerator;
